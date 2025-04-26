bcbc28f14c8bdfc4b7572d76d448ecb69cc4eda0
Kishan Sharma <kshar832@gmail.com>
2025-04-26 21:45:15 +0100

    fix ts warnings

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,7 +1,6 @@
 import Faq from '@/app/components/faq/faq';
 import React from 'react';
 import PageHeader from '@/app/components/page-header';
-import HighlightedHeading from '@/app/components/highlighted-heading';
 
 export default function FAQ() {
   return (

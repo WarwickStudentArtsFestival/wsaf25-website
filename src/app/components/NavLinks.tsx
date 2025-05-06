0ca9fa99a26c34d361b1ad0787e135c59b5e4d12
Kishan Sharma <kshar832@gmail.com>
2025-05-06 12:58:38 +0100

    make dev deployable again

--- src/app/components/NavLinks.tsx
+++ src/app/components/NavLinks.tsx
@@ -8,12 +8,23 @@ interface Props {
 export default function NavLinks({ onClick }: Props) {
   const pathname = usePathname();
 
-  const scheduleIsActive = pathname === '/schedule';
+  // const scheduleIsActive = pathname === '/schedule';
+  const performIsActive = pathname === '/perform';
   const crewIsActive = pathname === '/crew';
 
   return (
     <ul className="flex flex-row gap-4 md:gap-8 font-semibold uppercase">
       <li className="flex items-center justify-center gap-2">
+        <Link
+          href="/perform"
+          onClick={onClick}
+          className={performIsActive ? 'text-yellow-400' : ''}
+        >
+          Perform
+          <span className="xs:inline-block hidden">&nbsp;or Exhibit</span>
+        </Link>
+      </li>
+      {/* <li className="flex items-center justify-center gap-2">
         <Link
           href="/schedule"
           onClick={onClick}
@@ -22,7 +33,7 @@ export default function NavLinks({ onClick }: Props) {
           <span className="xs:inline-block hidden">View the&nbsp;</span>
           Schedule
         </Link>
-      </li>
+      </li> */}
       <li className="flex items-center justify-center gap-2">
         <Link
           href="/crew"
--- src/app/schedule/components/ErrorMessage.tsx
+++ src/app/schedule/components/ErrorMessage.tsx
@@ -10,10 +10,11 @@ export default function ErrorMessage() {
       <HighlightedHeading text="W-please-set-the-api-token" />
       <Image
         src="/rotating.gif"
-        className='mx-auto'
+        className="mx-auto"
         alt="loading sundae"
         width={500}
         height={500}
+        unoptimized
       />
       <div className="bg-red-100 border border-red-300 mb-4 w-fit mx-auto text-red-800 p-4 rounded">
         Failed to fetch Pretalx API.<br></br>Please set the API key in{' '}

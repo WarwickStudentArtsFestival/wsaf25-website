188f9d905e3f2739f4123f5ce090acff0dca4bda
Kishan Sharma <kshar832@gmail.com>
2025-05-06 15:21:39 +0100

    time and date

--- src/app/schedule/components/TalkCard.tsx
+++ src/app/schedule/components/TalkCard.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { FiMapPin, FiClock, FiUsers, FiList } from 'react-icons/fi';
+import {
+  FiMapPin,
+  FiClock,
+  FiCalendar,
+  FiUsers,
+  FiArrowRight,
+} from 'react-icons/fi';
 import { Talk } from '../lib/types';
 import Link from 'next/link';
 import TrackIcon from './TrackIcon';
@@ -9,15 +15,36 @@ type TalkCardProps = {
   id: number;
 };
 
+const formatDate = (time: string) => {
+  const date = new Date(time);
+  return date.toLocaleDateString([], {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
+};
+
+function formatDuration(durationInMinutes: number): string {
+  const hours = Math.floor(durationInMinutes / 60);
+  const minutes = durationInMinutes % 60;
+
+  if (hours === 0) {
+    return `${minutes} min`;
+  }
+
+  return minutes > 0 ? `${hours}h ${minutes} min` : `${hours}h`;
+}
+
 export default function TalkCard({ talk, id }: TalkCardProps) {
   return (
     <Link href={`/schedule/event/${talk.code}`}>
       <div
         key={`${talk.code}-${id}`}
-        className="border p-4 text-left border-slate-300 rounded-md overflow-hidden w-full h-full flex flex-col hover:scale-[1.02] transition duration-100 ease-in-out"
+        className="border p-4 text-left border-slate-300 rounded-md overflow-hidden w-full h-full flex flex-col hover:scale-[1.02] transition duration-150 ease-in-out shadow-lg"
       >
-        <p className="inline-flex items-center gap-2 text-sm font-medium rounded-full">
-          <span className=" p-2 bg-purple-200 text-purple-800 border border-purple-800 rounded-full">
+        <p className="inline-flex items-center gap-2 text-sm font-medium text-purple-700 rounded-full mb-4">
+          <span className="p-2 bg-purple-200 text-purple-800 border border-purple-800 rounded-full">
             <TrackIcon track={talk.track?.en} size={17} />
           </span>
           <span className="bg-purple-200 text-purple-800 border border-purple-800 rounded-full px-3 py-1">
@@ -25,46 +52,33 @@ export default function TalkCard({ talk, id }: TalkCardProps) {
           </span>
         </p>
 
-        <div className="p-4 flex flex-col flex-grow">
-          <h3 className="text-teal-700 text-xl font-semibold mb-2">
-            {talk.title}
-          </h3>
-          {talk.slot?.room && (
-            <p className="text-sm text-gray-700 flex items-center gap-1">
-              <FiMapPin className="inline text-gray-500" />
-              <strong>Room:</strong> {talk.slot.room.en}
-            </p>
-          )}
+        <div className="flex flex-col flex-grow">
+          <h3 className="text-teal text-xl font-semibold mb-3">{talk.title}</h3>
+
           {talk.slot?.start && (
-            <p className="text-sm text-gray-700 flex items-center gap-1">
-              <FiClock className="inline text-gray-500" />
-              <strong>Start:</strong>{' '}
-              {new Date(talk.slot.start).toLocaleTimeString([], {
-                hour: '2-digit',
-                minute: '2-digit',
-              })}
-            </p>
-          )}
-          {talk.track && (
-            <p className="text-sm text-gray-700 flex items-center gap-1">
-              <FiList className="inline text-gray-500" />
-              <strong>Track:</strong> {talk.track.en}
-            </p>
-          )}
-          {talk.speakers.length > 0 && (
-            <p className="text-sm text-gray-700 flex items-center gap-1">
-              <FiUsers className="inline text-gray-500" />
-              <strong>Speakers:</strong>{' '}
-              {talk.speakers.map((s) => s.name).join(', ')}
-            </p>
+            <>
+              <p className="text-sm text-gray-700 flex items-center gap-2 mb-1">
+                <FiCalendar className="text-purple-500" />
+                {formatDate(talk.slot.start)}
+              </p>
+              <p className="text-sm text-gray-700 flex items-center gap-2 mb-2">
+                <FiClock className="text-purple-500" />
+                {formatDuration(talk.duration)}
+              </p>
+            </>
           )}
 
-          {/* <p className="inline-flex items-center gap-1 px-3 py-1 w-fit text-sm font-medium text-orange bg-orange-100 rounded-full">
-            <FiTag className="text-purple-500" />
-            <span>
-              <strong>Type:</strong> {talk.submission_type.en}
-            </span>
-          </p> */}
+          {talk.slot?.room && (
+            <p className="text-sm text-gray-700 flex items-center gap-2 mb-2">
+              <FiMapPin className="text-purple-500" />
+              {talk.slot.room.en}
+            </p>
+          )}
+        </div>
+
+        <div className="flex items-center gap-2 mt-4 text-black text-sm font-medium">
+          <span>View Details</span>
+          <FiArrowRight className="text-purple-500" />
         </div>
       </div>
     </Link>

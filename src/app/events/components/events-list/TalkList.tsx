5d738ffd1f74b34ed68650e751c1435c26fb2ec2
Kishan Sharma <kshar832@gmail.com>
2025-05-11 20:28:53 +0100

    conditional scroll only when too many cards

--- src/app/events/components/events-list/TalkList.tsx
+++ src/app/events/components/events-list/TalkList.tsx
@@ -8,19 +8,25 @@ type TalkListProps = {
 };
 
 export default function TalkList({ talks }: TalkListProps) {
+  const TALKS_BEFORE_SCROLL = 4;
+
   if (!talks || talks.length === 0) {
     return <p>No events found. Maybe enable some filters?</p>;
   }
 
   return (
-    <div className="relative max-md:h-[500px] overflow-hidden">
-      <div className="max-md:h-[500px] w-full max-md:overflow-y-auto grid gap-2 grid-cols-2 md:grid-cols-3 xl:grid-cols-5 px-2">
-        {talks.map((talk, id) => (
-          <div key={`${talk.code}-${id}`} className="h-full w-full sm:p-2">
-            <TalkCard talk={talk} id={id} />
-          </div>
-        ))}
-      </div>
+    <div
+      className={`relative w-full grid gap-2 grid-cols-2 md:grid-cols-3 xl:grid-cols-5 px-2 ${
+        talks.length > TALKS_BEFORE_SCROLL
+          ? 'max-h-[500px] overflow-y-auto'
+          : 'h-auto'
+      }`}
+    >
+      {talks.map((talk, id) => (
+        <div key={`${talk.code}-${id}`} className="w-full sm:p-2">
+          <TalkCard talk={talk} id={id} />
+        </div>
+      ))}
     </div>
   );
 }

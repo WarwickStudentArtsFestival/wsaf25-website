422830773cdba06c85dfe866567af7303e37ebbe
Kishan Sharma <contact@kishan.app>
2025-05-13 08:20:24 +0100

    show more events before scroll enables

--- src/app/events/components/events-list/TalkList.tsx
+++ src/app/events/components/events-list/TalkList.tsx
@@ -8,19 +8,18 @@ type TalkListProps = {
 };
 
 export default function TalkList({ talks }: TalkListProps) {
-  const TALKS_BEFORE_SCROLL = 4;
-
   if (!talks || talks.length === 0) {
     return <p>No events found. Maybe enable some filters?</p>;
   }
 
   return (
     <div
-      className={`relative w-full grid gap-2 grid-cols-2 md:grid-cols-3 xl:grid-cols-5 px-2 ${
-        talks.length > TALKS_BEFORE_SCROLL
-          ? 'max-h-[500px] overflow-y-auto'
-          : 'h-auto'
-      }`}
+      className={`
+        relative w-full grid gap-2
+        grid-cols-2 md:grid-cols-3 xl:grid-cols-5 px-2
+        ${talks.length > 4 ? 'overflow-y-auto' : ''}
+        max-h-[calc(6*10rem)] sm:max-h-[calc(6*10rem)]
+      `}
     >
       {talks.map((talk, id) => (
         <div key={`${talk.code}-${id}`} className="w-full sm:p-2">

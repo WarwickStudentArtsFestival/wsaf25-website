c038fc63b196b7cfb6e73afac3937501f6ad0d19
Kishan Sharma <contact@kishan.app>
2025-05-26 11:42:16 +0100

    make event page more responsive

--- src/app/events/[slug]/page.tsx
+++ src/app/events/[slug]/page.tsx
@@ -36,76 +36,74 @@ export default async function Page({
   // const darkBg = `${trackColor}15`;
 
   return (
-    <div>
+    <div className="-mb-2">
       <Toaster position="top-center" />
       <PageHeader />
-      <div className="max-w-4xl my-8 mx-auto md:px-4 sm:px-6 lg:px-8">
-        <div className="mb-4">
-          <div
-            style={{ backgroundColor: lightBg }}
-            className="bg-white p-6 py-0 mb-4 h-fit rounded-lg  md:border md:border-gray-200"
-          >
-            <div style={{ color: trackColor }}>
-              <TalkHeader track={event.categoryPretalxTrack} />
-            </div>
+      <div className="max-w-4xl sm:my-8 -my-2 mx-auto md:px-4 sm:px-6 lg:px-8">
+        <div
+          style={{ backgroundColor: lightBg }}
+          className="bg-white p-6 py-0 h-fit rounded-lg  md:border md:border-gray-200"
+        >
+          <div style={{ color: trackColor }}>
+            <TalkHeader track={event.categoryPretalxTrack} />
+          </div>
 
+          <div className="my-4">
+            {/* <PresentedBy speakers={event.speakers} /> */}
+            <h1
+              style={{ color: trackColor }}
+              className="text-4xl font-bold break-words px-2 -mx-6 sm:mx-auto "
+            >
+              &ldquo;{event.name}&rdquo;
+            </h1>
+          </div>
+          {event.image && (
             <div className="my-4">
-              {/* <PresentedBy speakers={event.speakers} /> */}
-              <h1
-                style={{ color: trackColor }}
-                className="text-4xl font-bold break-words px-2 -mx-6 sm:mx-auto "
-              >
-                &ldquo;{event.name}&rdquo;
-              </h1>
+              <Image
+                src={event.image}
+                alt={`${event.name} presentation image`}
+                width={800}
+                height={600}
+                className="w-full max-h-96 object-contain rounded-lg"
+                priority
+              />
             </div>
-            {event.image && (
-              <div className="my-4">
-                <Image
-                  src={event.image}
-                  alt={`${event.name} presentation image`}
-                  width={800}
-                  height={600}
-                  className="w-full max-h-96 object-contain rounded-lg"
-                  priority
-                />
-              </div>
-            )}
+          )}
 
-            <div className="flex flex-col mb-4 text-left lg:flex-row gap-6">
-              <div className="lg:w-2/3 md:p-4">
-                <h2 className="text-black text-xl font-semibold mb-4">
-                  Description
-                </h2>
-                <div
-                  className="prose text-gray-700 max-w-none"
-                  dangerouslySetInnerHTML={{ __html: event.description }}
-                />
-                <div className="hidden lg:block">
-                  <h2 className="text-black text-xl font-semibold my-4">
-                    Related Events
-                  </h2>
-                  <div style={{ color: trackColor }}>
-                    <GoToVenue eventWithSessions={event} />
-                    <GoToGenre eventWithSessions={event} />
-                  </div>
-                </div>
-              </div>
+          <div className="flex flex-col mb-4 text-left lg:flex-row gap-6">
+            <div className="lg:w-2/3 md:p-4">
+              <h2 className="text-black text-xl font-semibold mb-4">
+                Description
+              </h2>
               <div
-                style={{ color: trackColor }}
-                className="lg:w-1/3 flex flex-col gap-4 md:p-4 md:pl-0"
-              >
-                {/* <div style={{ backgroundColor: darkBg }}> */}
-                <EventDetails eventWithSessions={event} />
-                {/* </div> */}
-                <Share talk={event} />
-              </div>
-              <div className="block lg:hidden">
+                className="prose text-gray-700 max-w-none"
+                dangerouslySetInnerHTML={{ __html: event.description }}
+              />
+              <div className="hidden lg:block">
                 <h2 className="text-black text-xl font-semibold my-4">
                   Related Events
                 </h2>
-                <GoToVenue eventWithSessions={event} />
-                <GoToGenre eventWithSessions={event} />
+                <div style={{ color: trackColor }}>
+                  <GoToVenue eventWithSessions={event} />
+                  <GoToGenre eventWithSessions={event} />
+                </div>
               </div>
+            </div>
+            <div
+              style={{ color: trackColor }}
+              className="lg:w-1/3 flex flex-col gap-4 md:p-4 md:pl-0"
+            >
+              {/* <div style={{ backgroundColor: darkBg }}> */}
+              <EventDetails eventWithSessions={event} />
+              {/* </div> */}
+              <Share talk={event} />
+            </div>
+            <div className="block lg:hidden">
+              <h2 className="text-black text-xl font-semibold my-4">
+                Related Events
+              </h2>
+              <GoToVenue eventWithSessions={event} />
+              <GoToGenre eventWithSessions={event} />
             </div>
           </div>
         </div>

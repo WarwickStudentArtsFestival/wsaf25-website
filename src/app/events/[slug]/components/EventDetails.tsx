c43ba87cf105299db676567acce293226d951948
Kishan Sharma <contact@kishan.app>
2025-05-26 16:31:21 +0100

    grid layout for sessions in events page

--- src/app/events/[slug]/components/EventDetails.tsx
+++ src/app/events/[slug]/components/EventDetails.tsx
@@ -11,46 +11,48 @@ export default function EventDetails({ eventWithSessions }: EventDetailsProps) {
   return (
     <div className="flex-1 h-fit text-left w-full overflow-hidden border bg-white border-gray-200 p-4">
       <h2 className="text-black text-xl font-semibold mb-4">Event Details</h2>
-      {eventWithSessions.sessions.map((session, idx) => {
-        const talkDetails = [
-          {
-            icon: <FiMapPin className="h-5 w-5" />,
-            title: 'Venue',
-            value: session.venueName || 'TBD',
-          },
-          {
-            icon: <FiCalendar className="h-5 w-5" />,
-            title: 'Date',
-            value: session.start ? formatDate(session.start) : 'TBD',
-          },
-          {
-            icon: <FiClock className="h-5 w-5" />,
-            title: 'Time',
-            value:
-              session.start && session.end  
-                ? `${formatTime(session.start)} - ${formatTime(session.end)}`
-                : 'TBD',
-          },
-        ];
+      <div className="grid grid-cols-2 lg:grid-cols-1">
+        {eventWithSessions.sessions.map((session, idx) => {
+          const talkDetails = [
+            {
+              icon: <FiMapPin className="h-5 w-5" />,
+              title: 'Venue',
+              value: session.venueName || 'TBD',
+            },
+            {
+              icon: <FiCalendar className="h-5 w-5" />,
+              title: 'Date',
+              value: session.start ? formatDate(session.start) : 'TBD',
+            },
+            {
+              icon: <FiClock className="h-5 w-5" />,
+              title: 'Time',
+              value:
+                session.start && session.end
+                  ? `${formatTime(session.start)} - ${formatTime(session.end)}`
+                  : 'TBD',
+            },
+          ];
 
-        return (
-          <div key={idx} className="mb-6">
-            {eventWithSessions.sessions.length > 1 && (
-              <h3 className="text-lg font-semibold text-gray-800 mb-2">
-                Session {idx + 1}
-              </h3>
-            )}
-            {talkDetails.map((detail, index) => (
-              <div key={index} className="flex items-center gap-4 mb-2">
-                <div>{detail.icon}</div>
-                <div className="text-left flex-grow">
-                  <p className="text-sm text-black">{detail.value}</p>
+          return (
+            <div key={idx} className="mb-6 ">
+              {eventWithSessions.sessions.length > 1 && (
+                <h3 className="text-lg font-semibold text-gray-800 mb-2">
+                  Session {idx + 1}
+                </h3>
+              )}
+              {talkDetails.map((detail, index) => (
+                <div key={index} className="flex items-center gap-4 mb-2">
+                  <div>{detail.icon}</div>
+                  <div className="text-left flex-grow">
+                    <p className="text-sm text-black">{detail.value}</p>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        );
-      })}
+              ))}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }

111094d0227081e78850fa869b09e7d9f2c0c7b0
Kishan Sharma <contact@kishan.app>
2025-05-27 13:00:32 +0100

    fix go to genre

--- src/app/events/[slug]/components/GoToGenre.tsx
+++ src/app/events/[slug]/components/GoToGenre.tsx
@@ -1,11 +1,10 @@
-import { trackTypes } from '@/lib/trackTypes';
+import { trackTypes, trackColourMap } from '@/lib/trackTypes';
 import { EventWithSessions } from '@/lib/events';
 import Link from 'next/link';
 import { FaArrowRight } from 'react-icons/fa';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import TrackPill from '@/app/components/track/TrackPill';
-import { trackColourMap } from '@/lib/trackTypes';
-
+import { eventCategories } from '@/data/events';
 
 export default async function GoToGenre({
   eventWithSessions,
@@ -15,33 +14,33 @@ export default async function GoToGenre({
   if (!eventWithSessions) {
     return <ErrorMessage msg="Event not found" />;
   }
-  const index = trackTypes.indexOf(eventWithSessions.categoryPretalxTrack);
-  const rawTrack = eventWithSessions.categoryPretalxTrack;
-  const trackKey = rawTrack.replace(/\s/g, '');
+
+  const track = eventWithSessions.categoryPretalxTrack;
+  const category = eventCategories.find((c) => c.pretalxTrack === track);
+
+  if (!category) {
+    return <ErrorMessage msg="Track category not found" />;
+  }
+
+  const bitFieldValue = (1 << category.filterBitFieldIndex).toString();
+  const trackKey = track.replace(/\s/g, '');
   const trackColor = trackColourMap[trackKey] || '#000';
 
   return (
     <div className="my-4 bg-white p-4 h-fit rounded-lg shadow-lg border border-gray-200 hover:scale-105 transition duration-100 ease-in-out">
       <Link
-        href={`/events?genre=${2 ** index}`}
+        href={`/events?category=${bitFieldValue}`}
         passHref
-        className=" cursor-pointer group"
-        title={`View more ${eventWithSessions.categoryPretalxTrack}`}
+        className="cursor-pointer"
+        title={`View more ${track}`}
       >
         <div className="flex items-center justify-center gap-2">
-          <TrackPill
-            showName={false}
-            track={eventWithSessions.categoryPretalxTrack}
-            padding={13}
-            size={25}
-          />
-          <div className="flex flex-col text-left m-2 items-start gap-1  font-medium ">
+          <TrackPill showName={false} track={track} padding={13} size={25} />
+          <div className="flex flex-col text-left m-2 items-start gap-1 font-medium">
             <span className="italic pt-2 text-black font-semibold text-lg -my-2">
               See more...
             </span>
-            <span className="font-semibold text-black pt-0">
-              {eventWithSessions.categoryPretalxTrack}
-            </span>
+            <span className="font-semibold text-black pt-0">{track}</span>
           </div>
           <div
             className="flex-1 flex justify-end pr-4"

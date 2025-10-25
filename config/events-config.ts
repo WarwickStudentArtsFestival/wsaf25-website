404177fac8449498e38dc5aee1c47656c10c9e30
Josh Heng <josh@joshheng.co.uk>
2025-10-25 12:58:56 +0100

    Move event filter defaults to central config

--- config/events-config.ts
+++ config/events-config.ts
@@ -2,6 +2,18 @@ import { EventsConfig } from '@config/types/events.config';
 
 const eventsConfig: EventsConfig = {
   enabled: true,
+
+  defaultFilters: {
+    // Whether to filter future events based on the current time by default.
+    // Should be enabled during WSAF and disabled afterwards.
+    filterByCurrentTime: false,
+
+    // list or timeline
+    view: 'list',
+
+    // random, time or venue
+    sort: 'random',
+  },
 };
 
 export default eventsConfig;
--- config/types/events.config.d.ts
+++ config/types/events.config.d.ts
@@ -1,3 +1,15 @@
 export type EventsConfig = {
   enabled: boolean;
+
+  defaultFilters: {
+    // Whether to filter future events based on the current time by default.
+    // Should be enabled during WSAF and disabled afterwards.
+    filterByCurrentTime: boolean;
+
+    // list or timeline
+    view: 'list' | 'timeline';
+
+    // random, time or venue
+    sort: 'random' | 'time' | 'venue';
+  };
 };

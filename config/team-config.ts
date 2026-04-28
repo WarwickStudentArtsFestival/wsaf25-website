59d4f7afc396ad2e5f299a800fa346db5fbaf7b9
Echo <97310557+DinoExho@users.noreply.github.com>
2026-04-28 19:25:34 +0100

    Change image to PNG

--- config/team-config.ts
+++ config/team-config.ts
@@ -17,7 +17,7 @@ import MattAdcock from '@/assets/team/matt-adcock.jpg';
 import MaximusKaiusLeighton from '@/assets/team/maximus-kaius-leighton.jpg';
 import KitCalvert from '@/assets/team/kit-calvert.jpg';
 import JoelCoulon from '@/assets/team/joel-coulon.jpg';
-import JonathanOrd from '@/assets/team/jonathan-ord.jpg';
+import JonathanOrd from '@/assets/team/jonathan-ord.png';
 import GeorgeParrett from '@/assets/team/george-parrett.jpg';
 import RowanMcAlpine from '@/assets/team/rowan-mcalpine.jpg';
 import AliceRobson from '@/assets/team/alice-robson.jpg';
